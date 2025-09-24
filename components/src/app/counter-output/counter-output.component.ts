8915ce9b222cc964b657a4920be8256e6f99f669
Ronny B Good <ronny.bruhin+bbc@hotmail.com>
2025-09-24 16:34:47 +0200

    686 | Standalone Components

--- components/src/app/counter-output/counter-output.component.ts
+++ components/src/app/counter-output/counter-output.component.ts
@@ -1,28 +1,17 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 
-import { CounterService } from '../counter.service';
+import { CounterService } from "../counter.service";
+import { Store } from "@ngrx/store";
 
 @Component({
-  selector: 'app-counter-output',
-  templateUrl: './counter-output.component.html',
-  styleUrls: ['./counter-output.component.css'],
+  selector: "app-counter-output",
+  templateUrl: "./counter-output.component.html",
+  styleUrls: ["./counter-output.component.css"],
 })
 export class CounterOutputComponent implements OnInit, OnDestroy {
   counter = 0;
   counterServiceSub?: Subscription;
 
-  constructor(private counterService: CounterService) {}
-
-  ngOnInit(): void {
-    this.counterServiceSub = this.counterService.counterChanged.subscribe(
-      (newVal) => (this.counter = newVal)
-    );
-  }
-
-  ngOnDestroy(): void {
-    if (this.counterServiceSub) {
-      this.counterServiceSub.unsubscribe();
-    }
-  }
+  constructor(private store: Store) {}
 }
