26a10421d49b20803950d4e0ee779e2abe9fd0bd
Ronny B Good <ronny.bruhin+bbc@hotmail.com>
2025-09-25 11:42:03 +0200

    692 | Alternative Way of Defining Actions

--- modules/src/app/store/counter.actions.ts
+++ modules/src/app/store/counter.actions.ts
@@ -1,8 +1,24 @@
-import { createAction, props } from "@ngrx/store";
+import { Action, createAction, props } from "@ngrx/store";
 
-export const increment = createAction(
-  "[Counter] Increment",
-  props<{ value: number }>()
-);
+// export const increment = createAction(
+//   "[Counter] Increment",
+//   props<{ value: number }>()
+// );
+
+export const INCREMENT = '[Counter] Increment';
+
+export class IncrementAction implements Action {
+  readonly type = INCREMENT;
+
+  constructor(public value: number) {}
+}
+
+// export class DecrementAction implements Action {
+//     readonly type = '[Counter] Increment';
+
+//     constructor(public value: number)  {}
+// }
 
 export const decrement = createAction("[Counter] Decrement");
+
+export type CounterActions = IncrementAction;
